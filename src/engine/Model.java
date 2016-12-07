583427de9580754348ec7bae9b35d96af62adfe6
lidalida <lidka.wojciechowska1996@gmail.com>
2016-12-07 23:49:24 +0100

    new metods in Model

--- src/engine/Model.java
+++ src/engine/Model.java
@@ -1,9 +1,13 @@
 package engine;
 
+import java.awt.Image;
 import java.beans.PropertyChangeListener;
 
 public interface Model {
 	void addPropertyChangeListener(PropertyChangeListener l);
 	void firePropertyChange(String propertyName,Object oldValue, Object newValue);
-	
+	Image getImage();
+	double getRotation();
+	int getX();
+	int getY();
 }
