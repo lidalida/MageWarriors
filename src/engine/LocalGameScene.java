24e2ba7b57c19cdcf851a19ab1b8177758aa1646
lidalida <lidka.wojciechowska1996@gmail.com>
2017-01-11 11:57:17 +0100

    LocalGameScene

--- src/engine/LocalGameScene.java
+++ src/engine/LocalGameScene.java
@@ -1,5 +1,43 @@
 package engine;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class LocalGameScene {
+	
+	private List<Drawable> models = new ArrayList<Drawable>();
+	private Model m;
+	
+	
+	public Drawable findModelByID(int i)
+	{
+		for(Iterator<Drawable> it = models.iterator(); it.hasNext(); )
+		{
+			m=(Model)it.next();
+			if(m.getID()==i)
+				return (Drawable)m;
+		}
+		return null;
+	}
+	
+	public void addModel(Drawable model)
+	{
+		models.add(model);
+	}
+	
+	public void removeModel(int i)
+	{
+		for(Iterator<Drawable> it = models.iterator(); it.hasNext(); )
+		{
+			m=(Model)it.next();
+			if(m.getID()==i)
+			{
+				it.remove();
+				models.remove(m);
+				m=null;
+			}
+		}
+	}
 
 }
